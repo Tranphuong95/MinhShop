1540b5fb798a28a01574bf08935d83327a1b5acf
Phuong <phuongphi407@gmail.com>
2021-03-23 02:01:20 +0700

    thêm trả lời nhận xét và sửa code
    Signed-off-by: Phuong <phuongphi407@gmail.com>

--- src/main/webapp/app/page-product/laptop/laptop-detail.tsx
+++ src/main/webapp/app/page-product/laptop/laptop-detail.tsx
@@ -16,10 +16,12 @@ export  const Comment=()=>{
     comment:'',
     name:'',
     email:'',
-    phoneNumber:''
-
+    phoneNumber:'',
+    children: []
   })
-  const [dataComment, setDataComment]=useState([])
+  const [dataComment, setDataComment]=useState([]);
+  const [statusReply, setStatusReply]=useState(null);
+  const [idReply, setIdReply]=useState(null);
   const commentModal=document.getElementById('comment-modal')
   const onChangeComment=(event)=>{
     setComment({...comment, [event.target.name]: event.target.value})
@@ -40,77 +42,130 @@ export  const Comment=()=>{
   const onCloseModalComment=()=>{
     commentModal.style.display='none'
   }
+  const onReply=(id)=>{
+    window.console.log(id)
+    // setStatusReply(!statusReply?id:null)
+    setStatusReply(statusReply===id?null:id)
+    setIdReply(id);
+  }
+
   const onSubComment=()=>{
     const realTime = new Date()
     axios({
       method:'post',
       url:'http://localhost:4001/comments',
-      data: {comment, time: realTime}
+      data: {...comment, time: realTime, reply: idReply}
     })
       .then(res=>{
         if(res.statusText==="Created"){
           onCloseModalComment();
-          setComment({comment:'', name:'', email:'', phoneNumber:''})
+          setComment({comment:'', name:'', email:'', phoneNumber:'', children:[]})
+          setStatusReply(null)
+          setIdReply(null)
         }
       })
 
   }
-  window.console.log(comment.comment.length)
+
+  const nestedComment=()=>{
+    const commentMap={};
+    dataComment.forEach(item=>commentMap[item.id]=item);
+    dataComment.forEach(item=>{
+      if(item.reply!==null){
+        let alreadyReply=false;
+        const parent =commentMap[item.reply];
+        parent.children=parent.children||[]
+        parent.children.map(x=>{
+          if(x.id===item.id){
+            alreadyReply=true;
+          }
+        })
+        if(!alreadyReply){
+          parent.children.push(item)
+        }
+      }
+    })
+    return dataComment.filter(item => {
+      return item.reply === null;
+    });
+  }
+  const showComment=(comments)=>{
+    let items=null
+    items=comments.map(item=>{
+      const times=new Date(item.time)
+      window.console.log(times)
+      return(
+        <div key={item.id}>
+          <div className="area-comment">
+            <div className="d-flex comment mt-3">
+              <div className="avatar-commentber text-white">avatar</div>
+              <div className="comment-content ml-2">
+                <div className="d-flex">
+                  <div className="mr-3"><strong>{item.name}</strong></div>
+                  <div className="text-secondary">{`${times.toLocaleTimeString()} ${times.toLocaleDateString()}`}</div>
+                </div>
+                <div>{item.comment}</div>
+                <div className="reply text-primary" onClick={()=>onReply(item.id)}><span>Trả lời</span></div>
+              </div>
+            </div>
+            <div className="mt-2" style={{paddingLeft: '82px'}}>{statusReply===item.id?formComment():null}</div>
+          </div>
+          <div className="area-reply ml-5">{showComment(item.children)}</div>
+        </div>
+      )
+    })
+    return items
+  }
+  useEffect(()=>{
+    window.console.log(nestedComment())
+  })
+  const formComment=()=>{
+    return(
+      <AvForm onSubmit={onSubComment} autoComplete='off'>
+        <AvGroup>
+          <AvField type='textarea' name='comment' minLength={3} maxLength={200} value={comment.comment} onChange={onChangeComment}placeholder="Nhận xét không dài quá 200 ký tự"/>
+        </AvGroup>
+        <Button type="button" className="btn-danger text-white" onClick={onOpenModalComment}>Gửi nhận xét</Button>
+
+        <div id="comment-modal" className="comment-modal-box">
+          <div className="comment-modal-content d-flex justify-content-center">
+            <div className=" modal-content col-6">
+              <div className="d-flex justify-content-between">
+                <span><h3>Hoàn thành gửi nhận xét</h3></span>
+                <span className="close-modal d-flex justify-content-end" onClick={onCloseModalComment}><h3>&times;</h3></span>
+              </div>
+              <AvGroup>
+                <Label>Tên<span>(bắt buộc)</span>:</Label>
+                <AvInput type="text" name='name' value={comment.name} onChange={onChangeComment} required />
+              </AvGroup>
+              <span>Để nhận thông báo khi có trả lời. Hãy nhập email và số điện thoại (Không bắt buộc)</span>
+              <AvGroup>
+                <Label>
+                  Email:
+                </Label>
+                <AvInput type="mail" name='email' value={comment.email} onChange={onChangeComment}/>
+              </AvGroup>
+              <AvGroup>
+                <Label>
+                  Số điện thoại:
+                </Label>
+                <AvInput type="text" name='phoneNumber' value={comment.phoneNumber} onChange={onChangeComment}/>
+              </AvGroup>
+              <Button type="submit" className="btn-danger text-white">Gửi nhận xét</Button>
+            </div>
+          </div>
+        </div>
+      </AvForm>
+    )
+  }
+  window.console.log(nestedComment())
   return(
     <div className="d-flex justify-content-center mt-5">
       <div className="col-9">
         <h2 className="text-dark"><strong>Nhận xét ({dataComment?dataComment.length:0})</strong></h2>
-        <AvForm onSubmit={onSubComment} autoComplete='off'>
-          <AvGroup>
-            <AvField type='textarea' name='comment' minLength={3} maxLength={200} value={comment.comment} onChange={onChangeComment}placeholder="Nhận xét không dài quá 200 ký tự"/>
-          </AvGroup>
-          <Button type="button" className="btn-danger text-white" onClick={onOpenModalComment}>Gửi nhận xét</Button>
-
-          <div id="comment-modal" className="comment-modal-box">
-            <div className="comment-modal-content d-flex justify-content-center">
-              <div className=" modal-content col-6">
-                <div className="d-flex justify-content-between">
-                  <span><h3>Hoàn thành gửi nhận xét</h3></span>
-                  <span className="close-modal d-flex justify-content-end" onClick={onCloseModalComment}><h3>&times;</h3></span>
-                </div>
-                <AvGroup>
-                  <Label>Tên<span>(bắt buộc)</span>:</Label>
-                  <AvInput type="text" name='name' value={comment.name} onChange={onChangeComment} required />
-                </AvGroup>
-                <span>Để nhận thông báo khi có trả lời. Hãy nhập email và số điện thoại (Không bắt buộc)</span>
-                <AvGroup>
-                  <Label>
-                    Email:
-                  </Label>
-                  <AvInput type="mail" name='email' value={comment.email} onChange={onChangeComment}/>
-                </AvGroup>
-                <AvGroup>
-                  <Label>
-                    Số điện thoại:
-                  </Label>
-                  <AvInput type="text" name='phoneNumber' value={comment.phoneNumber} onChange={onChangeComment}/>
-                </AvGroup>
-                <Button type="submit" className="btn-danger text-white">Gửi nhận xét</Button>
-              </div>
-            </div>
-          </div>
-        </AvForm>
+        {formComment()}
         <div>
-          {dataComment?dataComment.map(item=>{
-            const times=new Date(item.time)
-            return(
-              <div key={item.id} className="d-flex comment mt-3">
-                <div className="avatar-commentber text-white">avatar</div>
-                <div className="comment-content ml-2">
-                  <div className="d-flex">
-                    <div className="mr-3"><strong>{item.comment.name}</strong></div>
-                    <div className="text-secondary">{`${times.toLocaleTimeString()} ${times.toLocaleDateString()}`}</div>
-                  </div>
-                  <div>{item.comment.comment}</div>
-                </div>
-              </div>
-            )}
-          ):(<div>không có comment nào</div>)}
+          {showComment(nestedComment())}
         </div>
       </div>
     </div>
